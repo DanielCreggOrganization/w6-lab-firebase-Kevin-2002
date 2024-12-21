00fbc2c922cffd281e48f779eefd12761e0d35f8
Kevin McShane <121559784+Kevin-2002@users.noreply.github.com>
2024-12-21 21:06:07 +0000

    init firebase with my credentials in environments folder and added them to my gitignore

--- lab6/src/environments/environment.ts
+++ lab6/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+      projectId: "lab6-9cc43",
+      appId: "1:914908041854:web:7023685bfdb3e1c97dbe2f",
+      storageBucket: "lab6-9cc43.firebasestorage.app",
+      apiKey: "AIzaSyBJAIaLVyr7zjCKnwKiHR2auESAADyXH3I",
+      authDomain: "lab6-9cc43.firebaseapp.com",
+      messagingSenderId: "914908041854",
+  },
 };
 
 /*
