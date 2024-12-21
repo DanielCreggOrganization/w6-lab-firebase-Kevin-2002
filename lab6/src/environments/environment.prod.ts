25c03b7d4e3f67383efc9a5422cdf2511b7ae170
Kevin McShane <121559784+Kevin-2002@users.noreply.github.com>
2024-12-21 21:24:34 +0000

    stop

---
+++ lab6/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    projectId: "lab6-9cc43",
+    appId: "1:914908041854:web:7023685bfdb3e1c97dbe2f",
+    storageBucket: "lab6-9cc43.firebasestorage.app",
+    apiKey: "AIzaSyBJAIaLVyr7zjCKnwKiHR2auESAADyXH3I",
+    authDomain: "lab6-9cc43.firebaseapp.com",
+    messagingSenderId: "914908041854",
+  },
+};
